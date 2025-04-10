cf64b9834d28516209ae3f2d660325b0fb991f80
Daniel Vianna <durds.vianna@gmail.com>
2025-04-10 17:51:23 -0300

    Configuração da aplicação Spring Boot e Kafka

---
+++ src/main/java/com/integration/kafka2crm/Kafka2CrmApplication.java
@@ -0,0 +1,12 @@
+package com.integration.kafka2crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka2CrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kafka2CrmApplication.class, args);
+    }
+} 
